e28341be25047570dc573c6e55f43a256bab987e
Sandesh-Ideal <66362155+Sandesh-Ideal@users.noreply.github.com>
2022-06-20 22:24:36 +0530

    Update BranchingUnit.v

--- source_code/BranchingUnit.v
+++ source_code/BranchingUnit.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    19:19:49 05/19/2022 
-// Design Name: 
-// Module Name:    BranchingUnit 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module BranchingUnit(
 	input reset,
 	input [31:0] a,
