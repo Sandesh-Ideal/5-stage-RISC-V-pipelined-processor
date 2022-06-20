25694a713d6700b4f79274fdccdd6262d30f8b80
Sandesh-Ideal <66362155+Sandesh-Ideal@users.noreply.github.com>
2022-06-20 22:20:14 +0530

    Update ex_mem.v

--- source_code/ex_mem.v
+++ source_code/ex_mem.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    18:41:08 05/13/2022 
-// Design Name: 
-// Module Name:    ex_mem 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module ex_mem(
 	input clk,
 	input reset,
