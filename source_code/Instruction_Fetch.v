a643c4848794f373cb70192c643428c8593cef63
Sandesh-Ideal <66362155+Sandesh-Ideal@users.noreply.github.com>
2022-06-20 22:22:41 +0530

    Update Instruction_Fetch.v

--- source_code/Instruction_Fetch.v
+++ source_code/Instruction_Fetch.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    12:15:57 05/13/2022 
-// Design Name: 
-// Module Name:    Instruction_Fetch 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module Instruction_Fetch(
     input clk,
     input reset,
