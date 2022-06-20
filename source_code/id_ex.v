5ff47011f072063463e525c236c4550bd626fa95
Sandesh-Ideal <66362155+Sandesh-Ideal@users.noreply.github.com>
2022-06-20 22:20:03 +0530

    Update id_ex.v

--- source_code/id_ex.v
+++ source_code/id_ex.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    17:13:49 05/13/2022 
-// Design Name: 
-// Module Name:    id_ex 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module id_ex(
 	input clk,
 	input reset,
