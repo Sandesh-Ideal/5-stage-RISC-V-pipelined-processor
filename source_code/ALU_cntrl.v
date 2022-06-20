116fb2be51efa4689bead202746b3bd6dccdc8a8
Sandesh-Ideal <66362155+Sandesh-Ideal@users.noreply.github.com>
2022-06-20 22:25:03 +0530

    Update ALU_cntrl.v

--- source_code/ALU_cntrl.v
+++ source_code/ALU_cntrl.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    13:52:08 05/13/2022 
-// Design Name: 
-// Module Name:    ALU_cntrl 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module ALU_cntrl(
 	input [1:0] ALUop,
 	input [2:0] func3,
