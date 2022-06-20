d3d78a48f90b41b1d2c84d22a8adea3702904d72
Sandesh-Ideal <66362155+Sandesh-Ideal@users.noreply.github.com>
2022-06-20 22:23:57 +0530

    Update FSMClockGenerator.v

--- source_code/FSMClockGenerator.v
+++ source_code/FSMClockGenerator.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    00:10:37 05/21/2022 
-// Design Name: 
-// Module Name:    FSMClockGenerator 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module FSMClockGenerator(
 	input clk,
 	input reset,
