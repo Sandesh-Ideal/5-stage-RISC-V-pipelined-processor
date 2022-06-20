d9e007523df15dc17005e05d8f006ad258a0fcdd
Sandesh-Ideal <66362155+Sandesh-Ideal@users.noreply.github.com>
2022-06-20 22:22:04 +0530

    Update MUX.v

--- source_code/MUX.v
+++ source_code/MUX.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    16:55:39 05/13/2022 
-// Design Name: 
-// Module Name:    MUX 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module MUX(
 	input [31:0] a,
 	input [31:0] b,
