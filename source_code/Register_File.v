05faaae10216c3a07b3f4df320d508bd1fa013be
Sandesh-Ideal <66362155+Sandesh-Ideal@users.noreply.github.com>
2022-06-20 22:21:07 +0530

    Update Register_File.v

--- source_code/Register_File.v
+++ source_code/Register_File.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    12:17:08 05/13/2022 
-// Design Name: 
-// Module Name:    Register_File 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module Register_File(
     input [4:0] Read_Reg_1,
     input [4:0] Read_Reg_2,
