d6a15c187fa537e2043710c48bf2172317053eb3
Sandesh-Ideal <66362155+Sandesh-Ideal@users.noreply.github.com>
2022-06-20 22:19:13 +0530

    Update mem_wb.v

--- source_code/mem_wb.v
+++ source_code/mem_wb.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    18:49:56 05/13/2022 
-// Design Name: 
-// Module Name:    mem_wb 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module mem_wb(
 	input clk,
 	input reset,
